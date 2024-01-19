03de6bdfcb9259da8a1ba797291e432ec3d78baa
gillian <gillianxu09@gmail.com>
2024-01-19 14:30:39 +0100

    add to about

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
                     <p className="removeSpacing">I am an aspiring program developer who is currently studying at Waterloo for Computer Science.</p>
                     <p className="removeSpacing">My experience lies in using React, Typescript, Bootstrap, SQL, Node.js, Express.js and C++.</p>
                     <p className="removeSpacing">Having used multiple media platforms, played multiple games, I started thinking about how each of these intricate applications worked. Now that I am studying Computer Science, I hope that through the challenges I will encounter, I hope to be able to develop these applications myself.</p>
-                    <p className="removeSpacing">In my free time, I enjoy playing frisbee, playing the piano, and spending time with friends. I would like to spend this year developing more side projects, which includes trying to create my own game!</p>
+                    <p className="removeSpacing">In my free time, I enjoy playing frisbee, playing the piano, and spending time with friends. I would like to spend this year developing more side projects, which includes trying to create my own game! Currently, I am doing an exchange in Norway, and one of my goals so to travel around Europe this term. :) </p>
                     <p className="removeSpacing">Feel free to check out my resume below.</p>
                 </div>
             </div>
